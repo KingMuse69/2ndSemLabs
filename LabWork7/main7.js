db35b858f58c774c31c084fd28ac7a2a598abe12
WonderOfU <lebilash@email.com>
2026-05-12 20:59:55 +0300

    integrate message broadcasting

--- LabWork7/main7.js
+++ LabWork7/main7.js
@@ -9,7 +9,7 @@ messenger.on('message', (data) => {
   console.log(`[Logger]: New message: "${data.text}" from ${data.user}`);
 });
 
-// Notifications
+// Notification
 const unsubscribeNotifications = messenger.on('message', (data) => {
   if (data.priority === 'high') {
     console.log(`[UI Notification]: Attention! Message from ${data.user}!`);
@@ -22,14 +22,13 @@ messenger.on('message', () => {
 });
 
 // Demonstration
-
 console.log("--- first (working) ---");
 messenger.emit('message', { user: 'Oleksii', text: 'Lab Work Almost Done', priority: 'low' });
 
 console.log("\n--- second (high priority) ---");
 messenger.emit('message', { user: 'System', text: 'Critical error in code!', priority: 'high' });
 
-// Demonstration of unsubscription
+// Demonstration of the unsubscribe option
 console.log("\n--- Unsubscribing UI notifications and sending a third message ---");
 unsubscribeNotifications();
 
